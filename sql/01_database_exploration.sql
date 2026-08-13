092ec98704aa6278bb9d5fc6925f84e945f3aba1
letmebesai <letmebesai@gmail.com>
2026-08-13 23:27:45 +0530

    Add database exploration queries

---
+++ sql/01_database_exploration.sql
@@ -0,0 +1,14 @@
+-- ============================================================
+-- Database Exploration
+-- Project: European Soccer Data Engineering
+-- Database: soccer_dw
+-- ============================================================
+
+-- Select the project database
+USE soccer_dw;
+
+-- Display all tables available in the database
+SHOW TABLES;
+
+SELECT name
+FROM country;
